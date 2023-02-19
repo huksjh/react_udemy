440508ae3c3dd18503ffd692affe351e5cd20c32
huks <wkdgur1209@gmaillcom>
2023-02-19 14:54:09 +0900

    폴더수정

--- src/components/Card.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Card.css";
-const Card = (props) => {
-	const classes = `Card ${props.className ? props.className : ""}`;
-	return <div className={classes}>{props.children}</div>;
-};
-
-export default Card;
