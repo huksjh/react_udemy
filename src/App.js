5bc9bd5ca70bc6ca1c0c1d5d723d0b2dca46b312
huks <wkdgur1209@gmaillcom>
2023-02-19 14:43:38 +0900

    Udemy study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Expenses from "./components/Expenses";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const expenses = [
+		{
+			id: "el-1",
+			title: "가나다라",
+			amount: 1201,
+			date: new Date(2023, 0, 12),
+		},
+		{
+			id: "el-2",
+			title: "마바사아자",
+			amount: 1202,
+			date: new Date(2023, 1, 15),
+		},
+		{
+			id: "el-3",
+			title: "차카타파하",
+			amount: 1203,
+			date: new Date(2023, 1, 20),
+		},
+		{
+			id: "el-4",
+			title: "우리나라",
+			amount: 1204,
+			date: new Date(2023, 0, 23),
+		},
+	];
+
+	return (
+		<div>
+			<h2>Let's get started!</h2>
+			<Expenses items={expenses} />
+		</div>
+	);
 }
 
 export default App;
